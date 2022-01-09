d116cb167a0a34a7686bcea85cab32872fb36e96
yangzhuohao <yangzhuohao@xbdbike.com>
2022-01-09 17:57:00 +0800

    1

--- src/main/java/extend/Son.java
+++ src/main/java/extend/Son.java
@@ -5,10 +5,10 @@ public class Son extends Parent{
     public void speak(){
         System.out.println("son+speak");
     }
-    public void cc(){
+    public void cc1(){
         System.out.println("son+speak");
     }
-    public void aa(){
+    public void aa123(){
         System.out.println("son+speak");
     }
 }
