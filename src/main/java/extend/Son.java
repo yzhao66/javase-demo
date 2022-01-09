4f6c26bd952a84e8e7b1843b8c4415a6097af77b
yangzhuohao <yangzhuohao@xibaoda.onaliyun.com>
2022-01-09 17:46:54 +0800

    1

--- src/main/java/extend/Son.java
+++ src/main/java/extend/Son.java
@@ -5,4 +5,10 @@ public class Son extends Parent{
     public void speak(){
         System.out.println("son+speak");
     }
+    public void cc1(){
+        System.out.println("son+speak");
+    }
+    public void aa(){
+        System.out.println("son+speak");
+    }
 }
