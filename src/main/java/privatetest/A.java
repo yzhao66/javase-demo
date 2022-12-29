292eac163832014f4127224b7ff06a2ae0ba2c3a
yangzhuohao <cttclouds@163.com>
2022-12-29 18:37:11 +0800

    add:consumer

---
+++ src/main/java/privatetest/A.java
@@ -0,0 +1,13 @@
+package privatetest;
+
+/**
+ * @author zoh66
+ * @Description
+ * @create 2022-12-29 6:34 PM
+ */
+public class A {
+
+    private String msg="try to access the private variable outside the class";
+
+    String a = "a";
+}
---
+++ src/main/java/privatetest/PrivateExample1.java
@@ -0,0 +1,17 @@
+package privatetest;
+
+/**
+ * @author zoh66
+ * @Description
+ * @create 2022-12-29 6:34 PM
+ */
+public class PrivateExample1 {
+
+    public static void main(String[] args) {
+        A a=new A();
+        System.out.println(a);
+//        System.out.println(a.msg);
+        System.out.println(a.a);
+        System.out.println("aaaaaa");
+    }
+}
