e0f769f7eb8d263644ef2eb8507c892064148abe
yangzhuohao <cttclouds@163.com>
2023-01-05 17:44:29 +0800

    add

---
+++ src/main/java/循坏依赖/Main.java
@@ -0,0 +1,26 @@
+package 循坏依赖;
+
+/**
+ * @author zoh66
+ * @Description
+ * @create 2023-01-05 5:43 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new A());
+    }
+}
+
+
+
+class A {
+    public A() {
+        new B();
+    }
+}
+
+class B {
+    public B() {
+        new A();
+    }
+}
